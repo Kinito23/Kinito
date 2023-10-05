89a2549a836570157cba879867f4eb69a8f6f488
Kinito23 <146988921+Kinito23@users.noreply.github.com>
2023-10-05 22:06:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const contentbody = document.querySelector(".content-body");
+const parent = document.querySelector(".parent");
+const child1 = document.querySelector(".child1");
+
+child1.addEventListener('click', () => {
+    parent.innerHTML = "CONTINUE THE PASSION SA PAGTUKDO MO MA LORIE, THE BEST KA KAY HINIHINGUHA MO TALAGA NA BUONG KLASE MAY MAARAMAN SA KUNG NANO TINUTUKDO MO NA SUBJECT, NAN DI KA NAGTUTUGOT NA MAY NABIBILIN NA STUDYANTE SA KLASE, NAN MABUOTON KA PA LIWAT, HANEP MABUOTON DAW. <br><br> HAPPY TEACHER'S DAY ULIT MA LORIE. <br><br>:)";
+    child1.innerHTML = "";
+});
+
+
+
+
+
+
+
+
